0497edfcab2f4de73c4d5167c99db7e85ed46a33
Noah Cooper <ncooper@twilio.com>
2023-01-20 15:40:19 -0500

    Scaffold Blackbaud Raiser's Edge NXT Destination

---
+++ packages/destination-actions/src/destinations/blackbaud-raisers-edge-nxt/createOrUpdateIndividualConstituent/index.ts
@@ -0,0 +1,19 @@
+import type { ActionDefinition } from '@segment/actions-core'
+import type { Settings } from '../generated-types'
+import type { Payload } from './generated-types'
+
+const action: ActionDefinition<Settings, Payload> = {
+  title: 'Create or Update Individual Constituent',
+  description: '',
+  fields: {},
+  //perform: (request, data) => {
+  perform: () => {
+    // Make your partner api request here!
+    // return request('https://example.com', {
+    //   method: 'post',
+    //   json: data.payload
+    // })
+  }
+}
+
+export default action
